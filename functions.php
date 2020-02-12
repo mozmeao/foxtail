1d3d97f69d7e7f107c14ceedf8663b1a4d3145d0
Dan Brown <slightlyoffbeat@gmail.com>
2020-02-11 20:55:00 -0800

    Adding ACF to theme (instead of plugin)

--- functions.php
+++ functions.php
@@ -156,6 +156,23 @@ require get_template_directory() . '/inc/template-functions.php';
 require get_template_directory() . '/inc/customizer.php';
 
 /**
+ * Advanced Custom Fields
+ */
+
+// Define path and URL to the ACF plugin.
+define( 'MY_ACF_PATH', get_stylesheet_directory() . '/inc/acf/' );
+define( 'MY_ACF_URL', get_stylesheet_directory_uri() . '/inc/acf/' );
+
+// Include the ACF plugin.
+include_once( MY_ACF_PATH . 'acf.php' );
+
+// Customize the url setting to fix incorrect asset URLs.
+add_filter('acf/settings/url', 'my_acf_settings_url');
+function my_acf_settings_url( $url ) {
+    return MY_ACF_URL;
+}
+
+/**
  * Load Jetpack compatibility file.
  */
 // if ( defined( 'JETPACK__VERSION' ) ) {
