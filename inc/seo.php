81fc3f1cfc4818be4c1599836c926a7b5fa996d2
Dan Brown <slightlyoffbeat@gmail.com>
2020-07-31 00:23:36 -0700

    Favicons

--- inc/seo.php
+++ inc/seo.php
@@ -147,6 +147,17 @@ function foxtail_seo() {
   <meta name="twitter:description" content="<?php echo esc_attr($description) ?>" />
   <meta name="twitter:image" content="<?php echo esc_attr($image) ?>" />
 
+  <!-- Favicons & Icons -->
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() . '/assets/images/favicons/apple-touch-icon.png' ?>">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() . '/assets/images/favicons/favicon-32x32.png' ?>">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() . '/assets/images/favicons/favicon-16x16.png' ?>">
+  <link rel="manifest" href="<?php echo get_template_directory_uri() . '/assets/images/favicons/site.webmanifest' ?>">
+  <link rel="mask-icon" href="<?php echo get_template_directory_uri() . '/assets/images/favicons/safari-pinned-tab.svg' ?>" color="#20123a">
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri() . '/assets/images/favicons/favicon.ico' ?>">
+  <meta name="msapplication-TileColor" content="#20123a">
+  <meta name="msapplication-config" content="<?php echo get_template_directory_uri() . '/assets/images/favicons/browserconfig.xml'  ?>">
+  <meta name="theme-color" content="#7542E5">
+
   <?php $output = ob_get_clean();
 
  
