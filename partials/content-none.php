f49d6b6762121a291ad934e34927a128a2d3d67a
Dan Brown <slightlyoffbeat@gmail.com>
2020-10-26 16:36:15 -0700

    styling page for when no search results are available

--- partials/content-none.php
+++ partials/content-none.php
@@ -10,12 +10,12 @@
 ?>
 
 <section class="no-results not-found">
-	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'foxtail' ); ?></h1>
-	</header><!-- .page-header -->
+  <header class="page-header ft-u-center-text ">
+    <h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'foxtail' ); ?></h1>
+  </header><!-- .page-header -->
 
-	<div class="page-content">
-		<?php
+  <div class="page-content">
+    <?php
 		if ( is_home() && current_user_can( 'publish_posts' ) ) :
 
 			printf(
@@ -33,19 +33,22 @@
 
 		elseif ( is_search() ) :
 			?>
-
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'foxtail' ); ?></p>
-			<?php
+    <p>
+      <?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'foxtail' ); ?>
+    </p>
+    <?php
 			get_search_form();
 
 		else :
 			?>
 
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'foxtail' ); ?></p>
-			<?php
+    <p>
+      <?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'foxtail' ); ?>
+    </p>
+    <?php
 			get_search_form();
 
 		endif;
 		?>
-	</div><!-- .page-content -->
+  </div><!-- .page-content -->
 </section><!-- .no-results -->
