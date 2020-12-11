480d80203bb20610ce59b707559c8f8ca1d2b654
Dan Brown <slightlyoffbeat@gmail.com>
2020-12-10 21:20:31 -0800

    small spacing fix for front page

--- front-page.php
+++ front-page.php
@@ -49,19 +49,19 @@ set_query_var('featured_post', $featured_post);
 <?php
 if( get_field('display_collection_4_en', 'option') ) {
   get_template_part('partials/collection/collection-small-cards', null, array('collectionNum' => '4'));
+  <div class="ft-l-space-unrelated"></div>
 }
 ?>
 
-<div class="ft-l-space-unrelated"></div>
 
 <!-- Collection #5 -->
 <?php
 if( get_field('display_collection_5_en', 'option') ) {
   get_template_part('partials/collection/collection-large-cards', null, array('collectionNum' => '5'));
+  <div class="ft-l-space-unrelated"></div>
 }
 ?>
 
-<div class="ft-l-space-unrelated"></div>
 
 
 <!-- Featured CTA -->
