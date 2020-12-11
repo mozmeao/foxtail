5e3f4a47a93bb97877c282ab6a540a9dfb8ac133
Dan Brown <slightlyoffbeat@gmail.com>
2020-12-10 20:09:17 -0800

    updating author page

---
+++ author.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Foxtail
+ */
+
+get_header();
+
+
+?>
+<div class="ft-l-content">
+  <main class="ft-c-post-list">
+    <div class="ft-l-container">
+
+      <?php if ( have_posts() ) : ?>
+
+      <?php
+
+				echo '<div class="ft-c-archive-header">';
+				echo get_avatar( get_the_author_meta('ID'), 100);
+				echo '<div>';
+				echo '<h2>' .  get_the_author() . '</h2>';
+				the_archive_description( '<div class="archive-description">', '</div>' );
+				echo '</div>';
+			
+			
+			?>
+    </div><!-- .ft-c-archive-header -->
+    <div class="ft-l-space-related"></div>
+    <div class="ft-c-post-list__wrap--three-column">
+
+      <?php
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+				
+				get_template_part('partials/card-excerpt'); 
+
+			endwhile;
+
+
+		else :
+
+			get_template_part( 'partials/content', 'none' );
+
+		endif;
+		?>
+
+    </div>
+    <?php foxtail_pagination(); ?>
+</div>
+</main>
+</div>
+
+<?php
+get_footer();
---
+++ partials/card-excerpt.php
@@ -0,0 +1,18 @@
+<section class="mzp-c-card ft-c-card--excerpt mzp-has-aspect-1-1">
+  <a class="mzp-c-card-block-link" href="<?php the_permalink(); ?>">
+    <div class="mzp-c-card-content">
+      <div class="mzp-c-card-content">
+        <?php $post->post_parent; ?>
+        <?php if (!is_search()) { ?>
+        <div class="ft-c-pill__wrap">
+          <div class="ft-c-pill"><?php echo foxtail_get_cat($post) ?></div>
+        </div>
+        <?php } ?>
+        <h2 class="mzp-c-card-title"><?php the_title(); ?></h2>
+        <p class="mzp-c-card-desc"><?php echo wp_trim_words( get_the_excerpt(), 20, '...' );?></p>
+        <span class="ft-c-more">Read More</span>
+
+      </div>
+    </div>
+  </a>
+</section>
