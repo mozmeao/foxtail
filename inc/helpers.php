c515159079aa34c142354e5eeb9fa33acd15d8dd
Dan Brown <slightlyoffbeat@gmail.com>
2021-05-18 17:08:43 -0700

    update to how primary category is handled

--- inc/helpers.php
+++ inc/helpers.php
@@ -27,9 +27,9 @@ function foxtail_current_url() {
 
 // This will return the primary category which is set by Yoast.
 // if Yoast isn't available, it will return the top category for the post.
-// $categories should be an array of categories
+// $category should be an array of categories
 function foxtail_primary_cat($post_id) {
-  $categories = get_the_category($post_id);
+  $category = get_the_category($post_id);
   if ( class_exists('WPSEO_Primary_Term') ) {
 
         // Show the post's 'Primary' category, if this Yoast feature is available, & one is set
@@ -37,42 +37,29 @@ function foxtail_primary_cat($post_id) {
     $wpseo_primary_term = $wpseo_primary_term->get_primary_term();
     $term = get_term( $wpseo_primary_term );
 
-      if ( is_wp_error( $term ) ) {
+        if ( is_wp_error( $term ) ) {
 
-        // Default to first category (not Yoast) if an error is returned
-        $category_display = $categories[0]->name;
-        $category_slug = $categories[0]->slug;
+            // Default to first category (not Yoast) if an error is returned
+            $category_display = $category[0]->name;
+            $category_slug = $category[0]->slug;
 
-      } else {
+        } else {
 
-        // Check if category has parent
-        $category_id = $term->term_id;
-        $category_term = get_category($category_id);
-
-        // if primary category is a child
-        if( $category_term->category_parent > 0 ) {
-
-          // Get parent category object
-          $parent = $category_term->parent;
-          $parent_object = get_category($parent);
-
-          // Set parent category variables
-          $category_display = $parent_object->name;
-          $category_slug = $parent_object->slug;
-
-          // if primary cateogry is a parent
-          } else {
-
-            // Yoast Primary category
+            // Set variables for category_display & category_slug based on Primary Yoast Term
+            $category_id = $term->term_id;
+            $category_term = get_category($category_id);
             $category_display = $term->name;
             $category_slug = $term->slug;
+
         }
-      }
-  } else {
-    // Default, display the first category in WP's list of assigned categories
-    $category_display = $categories[0]->name;
-    $category_slug = $categories[0]->slug;
-  }
+
+    } else {
+
+        // Default, display the first category in WP's list of assigned categories
+        $category_display = $category[0]->name;
+        $category_slug = $category[0]->slug;
+
+    }
   return $category_display;
 }
 ?>
