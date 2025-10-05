48b357fdb40f8f906a741bb6e48de42e81727f19
DAnn2012 <dannunziata@hotmail.it>
2025-10-05 22:26:35 +0200

    Update template-tags.php

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -60,14 +60,14 @@ if ( ! function_exists( 'foxtail_entry_footer' ) ) :
       $categories_list = get_the_category_list( esc_html__( ', ', 'list item separator', 'foxtail' ) );
       if ( $categories_list ) {
         /* translators: 1: list of categories. */
-        printf( '<span class="cat-links">' . esc_html_e('Posted in %1$s', 'foxtail') . '</span>', $categories_list ); // WPCS: XSS OK.
+        printf( '<span class="cat-links">' . esc_html__('Posted in %1$s', 'foxtail') . '</span>', $categories_list ); // WPCS: XSS OK.
       }
 
       /* translators: used between list items, there is a space after the comma */
       $tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'foxtail' ) );
       if ( $tags_list ) {
         /* translators: 1: list of tags. */
-        printf( '<span class="tags-links">' . esc_html_e('Tagged %1$s', 'foxtail') . '</span>', $tags_list ); // WPCS: XSS OK.
+        printf( '<span class="tags-links">' . esc_html__('Tagged %1$s', 'foxtail') . '</span>', $tags_list ); // WPCS: XSS OK.
       }
     }
 
